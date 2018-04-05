d50f418a7e827b54c9bd25adf722facde93e44d1
rsmekala <rsmekala@juniper.net>
2018-04-05 12:11:36 +0530

    Removing import_correction from action_plugins

--- library/juniper_junos_system.py
+++ library/juniper_junos_system.py
@@ -246,6 +246,7 @@ other_re:
 
 # From Ansible 2.1, Ansible uses Ansiballz for assembling modules
 # Ansiballz packages module_utils into ansible.module_utils
+from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils import juniper_junos_common
 
 def main():
